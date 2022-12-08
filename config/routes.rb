f4b5eec995db442ab8c1f5e5cd91a13827f3c09f
Carla Valdivia <valdivia.carla@gmail.com>
2022-12-08 12:01:43 -0300

    gardens and plants I

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :gardens
+  resources :plants, only: [:destroy]
+  resources :gardens do
+    resources :plants, only: [:new, :create]
+  end
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
